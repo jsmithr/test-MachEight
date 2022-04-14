f8740dc2530e140dda760404e6bc989208de769e
Johann Smith Reyes Zapata <johann.reyes@pragma.com.co>
2022-04-14 13:10:15 -0500

    Upload files to test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
 
+interface Player {
+    first_name: string,
+    h_in: number,
+    h_meters: number,
+    last_name: string,
+    pair?: Player[]
+}
+interface Data {
+    data: { values: Player[] },
+}
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-machEight';
+    title = 'test-machEight';
+    nbaPlayers: any[];
+    playersSort: Player[];
+    displayedColumns: string[] = ['position', 'name'];
+    h_in: number = 0;
+
+    constructor() {
+        axios.get('https://mach-eight.uc.r.appspot.com/').then(({ data }: Data) => {
+
+            this.playersSort = data.values.sort((playerA: Player, playerB: Player) => playerA.h_in - playerB.h_in);
+
+        });
+    }
+
+    getPairs() {
+        console.log(new Date().getTime());
+        let pairsPlayers = this.playersSort.map((player: Player) => {
+            if (player.h_in * 2 > this.h_in)
+                return false;
+
+            let pairs = this.playersSort.filter((p: Player) => p.first_name != player.first_name && p.last_name != player.last_name && (Number(p.h_in) + Number(player.h_in)) == this.h_in);
+            if (pairs.length == 0)
+                return false;
+
+            return { ...player, pairs };
+        }).filter(Boolean);
+
+        this.nbaPlayers = pairsPlayers;
+        console.log(new Date().getTime());
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        MatTableModule,
+        BrowserAnimationsModule,
+        MatCardModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        FormsModule,
+        CommonModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
